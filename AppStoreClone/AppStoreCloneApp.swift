b2703eb00169e3b376c5d3e1ab2b88e565637aca
imhans <hans78905@gmail.com>
2023-03-29 23:39:50 +0900

    first commit

---
+++ AppStoreClone/AppStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreCloneApp.swift
+//  AppStoreClone
+//
+//  Created by Hans Yim on 2023/03/29.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
